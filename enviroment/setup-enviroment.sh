fe9f6435c3c97669d0b21910badecd887ff23352
Add <addgamer09@gmail.com>
2024-01-22 12:49:13 -0600

    Update software installation scripts
    Add Chocolatey installation step
    Install Notion via conda-forge
    Update ngrok installation
    Remove MySQL and JDK 17 installation
    Add mongodb-shell installation
    Update media tools installation
    Remove MySQL and JDK 17 installation in setup-enviroment.sh
    Update ngrok installation in setup-enviroment.sh
    Remove JDK 17 installation and linking in setup-enviroment.sh
    Update anaconda installation in setup-enviroment.sh
    
    Refactor software installation scripts

--- enviroment/setup-enviroment.sh
+++ enviroment/setup-enviroment.sh
@@ -14,15 +14,6 @@ brew install git
 echo "Installing ngrok.."
 brew install ngrok/ngrok/ngrok
 
-echo "Installing MySQL..."
-brew install mysql
-
-echo "Installing JDK 17..."
-brew install openjdk@17 maven
-
-echo "Linking JDK 17..."
-sudo ln -sfn /usr/local/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
-
 echo "Installing anaconda..."
 brew install --cask anaconda
 
